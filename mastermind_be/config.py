a90389f26a726663ecf475a9cdb6cc21280cd9cb
MaltezC <cflux.maltez@live.com>
2023-11-06 23:14:03 -0800

    revised __init__.py and config. pulled from mnb.

--- mastermind_be/config.py
+++ mastermind_be/config.py
@@ -8,3 +8,6 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class DevelopmentConfig(Config):
+    DEBUG = True
